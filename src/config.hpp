12192245edf5c97384dad31f0704cb6de6849a60
Mathias Lyngbye <mathias@allesen.dk>
2025-06-22 21:09:05 +0200

    Let  config reflect development direction.

--- src/config.hpp
+++ src/config.hpp
@@ -1,12 +1,27 @@
 #ifndef CONFIG_HPP
 #define CONFIG_HPP
 
+#include <string>
+#include <array>
+
 #include "target.hpp"
 
+inline const std::array<std::array<std::string, 2>, 3> colors =  {{
+    /* fg        bg        */
+    { "#bbbbbb", "#222222" },
+    { "#eeeeee", "#005577" },
+    { "#000000", "#00ffff" }
+}};
+
+// Sources
 inline const std::vector<Target> targets = {
     /* Path                         depth       extensions*/
     {"C:\\Program Files",           1,          {".exe"}},
     {"C:\\Program Files (x86)",     1,          {".exe"}}
 };
+
+// Lines
+// If non-zero, use vertical line layout
+inline const unsigned int lines = 0;
 
 #endif
